fe315d77cbb6c248c565a1f520420d0349e4c320
Antriksh <antrix.sharma@gmail.com>
2015-02-04 16:09:12 -0500

    Initial Commit

---
+++ src/main/java/com/as/common/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package com.as.common.controllers;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+ 
+@Controller
+@RequestMapping(value="/")
+public class HomeController {
+ 
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+ 
+		model.addAttribute("message", "This is HOME");
+		return "home";
+ 
+	}
+ 
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+	<h1>Am I home? : ${message}</h1>	
+</body>
+</html>
