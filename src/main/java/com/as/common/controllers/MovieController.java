9ab905e7909b1d638ac370f6d29dca385abcb57d
Antriksh <antrix.sharma@gmail.com>
2015-02-04 22:01:31 -0500

    Initial commit

---
+++ src/main/java/com/as/common/controllers/MovieController.java
@@ -0,0 +1,29 @@
+package com.as.common.controllers;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+ 
+@Controller
+@RequestMapping(value="/movie")
+public class MovieController {
+ 
+	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+	public String getMovie(@PathVariable String name, ModelMap model) {
+ 
+		model.addAttribute("movie", name);
+		return "list";
+ 
+	}
+ 
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String getDefaultMovie(ModelMap model) {
+ 
+		model.addAttribute("movie", "this is default movie");
+		return "list";
+ 
+	}
+ 
+}
